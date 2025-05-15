ce6fbd386da323d4522ba254f169e4a00a4993a8
Sakshi Athare <atharesakshi590@gmail.com>
2025-05-15 20:32:54 +0530

    commit6

--- src/app/creditmanager/documentverification/documentverification.component.ts
+++ src/app/creditmanager/documentverification/documentverification.component.ts
@@ -11,39 +11,51 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./documentverification.component.css']
 })
 export class DocumentverificationComponent implements OnInit {
+  retrievedDoc: Docmodel[] = [];
+  verificationForm: FormGroup;
 
-  constructor(private commonService:CommonService, private actRout:ActivatedRoute, private formbuilder:FormBuilder, private loc:Location) { }
-  retrievedDoc!:Docmodel[]
-
-  verificationForm!:FormGroup;
+  constructor(
+    private commonService: CommonService, 
+    private actRout: ActivatedRoute, 
+    private formbuilder: FormBuilder, 
+    private loc: Location
+  ) { }
 
   ngOnInit(): void {
-    this.actRout.paramMap.subscribe(
-      param1=>
-      {
-      let id=parseInt(param1.get('documentid'));
-      this.commonService.getDocSingle(id).subscribe((list:Docmodel[])=>{this.retrievedDoc=list})
-      }
-    )
-
-    this.verificationForm=this.formbuilder.group({
-      addressProof:['', Validators.required],
-      itr:['',Validators.required],
-      pancard:['', Validators.required],
-      adharcard:['', Validators.required],
-      signature:['', Validators.required],
-      photo:['', Validators.required],
-      thumb:['', Validators.required],
-      bankcheck:['', Validators.required],
-      propertydetails:['', Validators.required],
-      salaryslip:['', Validators.required]
-
-    })
-
+    this.initializeForm();
+    this.loadDocumentData();
   }
-  back(){
+
+  private initializeForm(): void {
+    this.verificationForm = this.formbuilder.group({
+      addressProof: ['', Validators.required],
+      itr: ['', Validators.required],
+      pancard: ['', Validators.required],
+      adharcard: ['', Validators.required],
+      signature: ['', Validators.required],
+      photo: ['', Validators.required],
+      thumb: ['', Validators.required],
+      bankcheck: ['', Validators.required],
+      propertydetails: ['', Validators.required],
+      salaryslip: ['', Validators.required]
+    });
+  }
+
+  private loadDocumentData(): void {
+    this.actRout.paramMap.subscribe(param1 => {
+      const id = parseInt(param1.get('documentid'));
+      this.commonService.getDocSingle(id).subscribe(
+        (list: Docmodel[]) => {
+          this.retrievedDoc = list;
+        },
+        error => {
+          console.error('Error loading document data:', error);
+        }
+      );
+    });
+  }
+
+  back(): void {
     this.loc.back();
   }
-
-
 }
